43a167f18e516c4a797cd9abdb92b04a77393c31
MIqbal-shop <miqbal05a@gmail.com>
2026-08-23 16:56:04 +0500

    Delete public/config.js

--- public/config.js
+++
@@ -1,6 +0,0 @@
-// Edit these two lines to your own shop's name/tagline - that's it, nothing
-// else in the site needs changing to rebrand it.
-window.STORE_CONFIG = {
-  name: 'Aapki Dukaan',
-  tagline: 'Seedha order karein, ghar baithe',
-};
